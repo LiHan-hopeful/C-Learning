b87e19df5f2f35ecd37c2593726b86cd82da7ff1
LiHan-hopeful <1036177761@qq.com>
2019-11-21 09:49:07 +0800

    斐波那契数列（递归和非递归实现）

---
+++ LH-1121/LH-1121/test.c
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+//递归实现
+int Fib(int n) {
+	if (n == 1) {
+		return 1;
+	}
+	if (n == 2) {
+		return 1;
+	}
+	return Fib(n - 1) + Fib(n - 2);
+}
+//非递归实现
+int Fib2(int n) {
+	if (n == 1) {
+		return 1;
+	}
+	if (n == 2) {
+		return 1;
+	}
+	int last1 = 1;
+	int last2 = 1;
+	int current = 0;
+	for (int i = 3; i <= n; i++) {
+		current = last1 + last2;
+		last2 = last1;
+		last1 = current;
+	}
+	return current;
+}
+int main() {
+	int num = 0;
+	printf("请输入您要求出的数：\n");
+	scanf("%d", &num);
+	printf("第%d个数的值为：%d\n", num, Fib(num));
+	printf("第%d个数的值为：%d\n", num, Fib2(num));
+    system("pause");
+    return 0;
+}
